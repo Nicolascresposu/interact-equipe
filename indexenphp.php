00a7de8e0de85cc8f28741888458e5bcb33cd3d2
Your Name <nicolascresposuarez@gmail.com>
2021-11-16 04:19:48 -0400

    fixed header

--- indexenphp.php
+++ indexenphp.php
@@ -10,7 +10,47 @@
 </head>
 <body>
 <?php
-include "header.html";
+require "header.html";
+?>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/slideshow1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Al servicio de la comunidad</h5>
+        <p>Una comunidad unida es más fuerte</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/slideshow2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Trabajo en equipo</h5>
+        <p>Enseñamos a los más jovenes a trabajar con gente diferente a ellos.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/slideshow3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Ayuda en variedad</h5>
+        <p>Hemos estado por toda la ciudad; ¡Lee las historias de a los que hemos ayudado!</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<?php
 $tvar = "Esta es una variable de prueba";
 $array = array("almendra","papas","mayonesa");
 echo $tvar;
